3ab3def85c1079434721ebe3f58628c4a927da9f
Michael Hicks <hicks.michael04@gmail.com>
2023-01-26 17:50:36 -0700

    add logout feature specs

---
+++ spec/features/user/logout_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "Logout", :type => :feature do
+  let(:user) { create(:user) }
+
+   xit 'user successfully logs out', js: true do
+    # save_and_open_page
+    sign_in user
+    visit root_path
+    find('nav #user-settings').click
+    find('nav a', text: 'Log out').click
+    expect(page).to have_selector('nav a', text: 'Login')
+  end
+
+end
